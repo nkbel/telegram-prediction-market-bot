7c56f7555ee6369aaab04394afd03742a400de23
Nikita Belosludtcev <nik.beloslulcev@bk.ru>
2025-12-07 01:31:28 +0300

    Update API tunnel URL

--- webapp/src/config.js
+++ webapp/src/config.js
@@ -2,5 +2,5 @@
 export const API_URL = import.meta.env.VITE_API_URL || 
   (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
     ? 'http://localhost:3000' 
-    : 'https://three-doodles-shake.loca.lt'); // Fallback to tunnel URL for deployed version
+    : 'https://tangy-dots-attack.loca.lt'); // Fallback to tunnel URL for deployed version
 
